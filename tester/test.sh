0874d353b5f1e68aa1945df4185a0a3792245930
jweeeezy <jwillert@42heilbronn.student.de>
2023-08-20 15:34:21 +0200

    added simple tester

---
+++ tester/test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# variables
+MAP_DIR="../maps/"
+SOL_DIR="./solutions/"
+PROGRAM="../bsq"
+
+# script
+echo
+for MAP_FILE in "$MAP_DIR"*; do
+	# variables
+	BASE_NAME=$(basename "$MAP_FILE")
+	NUM=${BASE_NAME#map_}
+	SOL_FILE=sol_$NUM
+
+	OUTPUT=$($PROGRAM $MAP_FILE)
+	EXPECTED=$(cat $SOL_DIR$SOL_FILE)
+
+	echo -e "\033[1;33m$BASE_NAME:\033[0m"
+	if [ "$OUTPUT" == "$EXPECTED" ]; then
+		echo "✅"
+		echo
+	else
+		echo "$OUTPUT"
+		echo "$EXPECTED"
+		echo "❌"
+		echo
+	fi
+done
+echo
